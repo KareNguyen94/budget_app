15a56ad1d6e1b124ed62bef5ce22b9258ffe4717
Karen Nguyen <knguyen4184@yahoo.com>
2019-10-30 10:57:25 -0600

    Initial commit

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 var form = document.querySelector('form');
 var inputs = document.querySelectorAll('.typed-input');
 var cards = document.querySelector('.account-cards');
+var transactionArray = [];
+var type = inputs[0].value;
+var payee = inputs[2].value;
+var amount = inputs[3].value;
+var category = inputs[4].value;
 
-form.addEventListener('submit', addTransaction);
+form.addEventListener('submit', onFormClick);
 
-function addTransaction(event) {
-  event.preventDefault();
+window.addEventListener("load", pageLoad);
 
-  var type = inputs[0].value;
-  var payee = inputs[2].value;
-  var amount = inputs[3].value;
-  var category = inputs[4].value;
-
-  addCard(type, payee, amount, category);
+function pageLoad() {
+  if ("transaction" in localStorage) {
+     checkLocalStorage();
+  }
 }
 
-function addCard(type, payee, amount, category) {
+function onFormClick() {
+  instantiateCard(type.value, payee.value, amount.value, category.value);
+}
+
+function parseLocalStorage() {
+  var getItem = localStorage.getItem("transaction");
+  var storageArray = JSON.parse(getItem);
+  return storageArray;
+}
+
+function checkLocalStorage() {
+  var storageArray = parseLocalStorage();
+    for (var i = 0; i < storageArray.length; i++) {
+      instantiateCard(storageArray[i].type, storageArray[i].payee, storageArray[i].amount, storageArray[i].category);
+    }
+}
+
+function addCard(newTransaction) {
   cards.innerHTML += `
-    <section class="account-card">
-      <img class="account-icon" src="./assets/${type}.svg" alt="Expenses Icon">
+    <section id=${newTransaction.id} class="account-card">
+      <img class="account-icon" src="./assets/${newTransaction.type}.svg" alt="Expenses Icon">
       <div class="account-title">
-        <h3>${payee}</h3>
+        <h3>${newTransaction.payee}</h3>
       </div>
-      <p>${category}</p>
-      <h4>$${amount}</h4>
+      <p>${newTransaction.category}</p>
+      <h4>$${newTransaction.amount}</h4>
     </section>
   `;
+}
+
+function instantiateCard(type, payee, amount, category) {
+  event.preventDefault()
+  var transaction = new Transaction(type, payee, amount, category);
+  transactionArray.push(transaction);
+  addCard(transaction);
+  transaction.saveToStorage(transactionArray);
 }
--- transaction.js
+++ transaction.js
@@ -1,7 +1,14 @@
 class Transaction {
 
-  constructor() {
-
+  constructor(type, payee, amount, category, id) {
+    this.type = type;
+    this.payee = payee;
+    this.amount = amount;
+    this.category = category;
+    this.id = id || Date.now();
   }
-  
+
+  saveToStorage(array){
+    localStorage.setItem("transaction", JSON.stringify(array));
+  }
 }
